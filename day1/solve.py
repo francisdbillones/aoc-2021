49c21a457864ba8fbb64ab4bf0ae42ba4c1623d6
francisdbillones <francisd.billones@gmail.com>
2021-12-01 13:28:37 +0800

    day 1

--- day1/solve.py
+++ day1/solve.py
@@ -1,8 +1,23 @@
 from pathlib import Path
 
+Path("output.txt").write_text('')
+
 lines = Path("input.txt").read_text().split('\n')
+out = Path("output.txt")
+og_print = print
+
+def print(*args, **kwargs):
+	og_print(*args, **kwargs, file=out.open('a'))
 
 def solve():
-	pass
+	res = 0
+	nums = list(map(int, lines))
+	last = float("inf")
+
+	for a, b, c in zip(nums, nums[1:], nums[2:]):
+		if a + b + c > last:
+			res += 1
+		last = a + b + c
+	print(res)
 
 solve()
