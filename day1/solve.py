e36b84dcaf85058d31514e994cdf8dc009b95010
francisdbillones <francisd.billones@gmail.com>
2021-12-02 13:20:22 +0800

    day 2

--- day1/solve.py
+++ day1/solve.py
@@ -10,14 +10,7 @@ def print(*args, **kwargs):
 	og_print(*args, **kwargs, file=out.open('a'))
 
 def solve():
-	res = 0
-	nums = list(map(int, lines))
-	last = float("inf")
-
-	for a, b, c in zip(nums, nums[1:], nums[2:]):
-		if a + b + c > last:
-			res += 1
-		last = a + b + c
-	print(res)
+	nums = [*map(int,lines)]
+	print(sum(a<b for a, b in zip(nums, nums[3:])))
 
 solve()
---
+++ day2/solve.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+Path("output.txt").write_text("")
+
+lines = Path("input.txt").read_text().split("\n")
+out = Path("output.txt")
+og_print = print
+
+
+def print(*args, **kwargs):
+    og_print(*args, **kwargs, file=out.open("a"))
+
+
+def part1():
+    hor = 0
+    dep = 0
+
+    for line in lines:
+        com, mag = line.split()
+        mag = int(mag)
+        if com == "forward":
+            hor += mag
+        elif com == "up":
+            dep -= mag
+        elif com == "down":
+            dep += mag
+    print(hor * dep)
+
+
+def part2():
+    aim = 0
+    hor = 0
+    dep = 0
+
+    for line in lines:
+        com, mag = line.split()
+        mag = int(mag)
+
+        if com == "forward":
+            hor += mag
+            dep += aim * mag
+        elif com == "up":
+            aim -= mag
+        elif com == "down":
+            aim += mag
+    print(hor * dep)
+
+
+part1()
+part2()
